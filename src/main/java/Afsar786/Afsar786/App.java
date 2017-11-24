c1f5f7519424ca4dced0dcf2f578cb9e64f9e7a9
Afsar273 <upworkafsar@gmail.com>
2017-11-24 21:11:35 +0530

    Hello this is commit

---
+++ src/main/java/Afsar786/Afsar786/App.java
@@ -0,0 +1,13 @@
+package Afsar786.Afsar786;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
